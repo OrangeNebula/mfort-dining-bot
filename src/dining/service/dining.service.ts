d5906bb88dc1c924acef824d28d14200376dadb9
rednebula <rednebula01018@gmail.com>
2020-10-10 19:17:43 +0900

    [기능] 랜덤한 메뉴 제안 기능 추가

--- src/dining/service/dining.service.ts
+++ src/dining/service/dining.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { DiningRepository } from '../repository/dining.repository';
-import { Dining, Participant } from '../domain/dining.domain';
+import { Dining, Menu, Participant } from '../domain/dining.domain';
 import { SlackService } from '../../slack/slack.service';
 
 interface DiningMessageOptions {
   title?: string,
   expireTime?: string,
   enableButton?: boolean,
+}
+
+interface OrdererMessageOptions {
+  menu?: Menu,
 }
 
 @Injectable()
@@ -75,7 +79,9 @@ export class DiningService {
       throw new Error('오늘은 저녁 멤버가 없습니다.');
     }
     await this.diningRepository.updateDining(dining);
-    await this.slackService.postMessage([], `오늘 주문은 <@${orderer.user}> 님이 해주세요!`);
+    await this.slackService.postMessage(this.getOrdererMessage(orderer, {
+      menu: dining.menu,
+    }));
   }
 
   getDiningMessageBlocks(
@@ -130,5 +136,57 @@ export class DiningService {
         }
       }
     ]
+  }
+
+  getOrdererMessage(
+    orderer: Participant,
+    options?: OrdererMessageOptions,
+  ) {
+    const textMenuSuggestion = options?.menu?.name ? `\n 메뉴는 ${options.menu.name} 어때요? :smile:` : '';
+    let orderButtonBlock = null;
+    if (options.menu.link) {
+      orderButtonBlock = {
+        type: 'section',
+          text: {
+        type: 'mrkdwn',
+          text: '빠르게 주문하려면 오른쪽 버튼을 클릭해주세요!'
+      },
+        accessory: {
+          type: 'button',
+            text: {
+            type: 'plain_text',
+              text: '주문하기',
+              emoji: true
+          },
+          value: 'menu',
+            url: options.menu.link,
+            action_id: 'button-action'
+        }
+      };
+    }
+    let menuImageBlock = null;
+    if (options.menu.imgUrl) {
+      menuImageBlock = {
+        type: 'image',
+        title: {
+          type: 'plain_text',
+          text: options.menu.name,
+          emoji: true
+        },
+        image_url: options.menu.imgUrl,
+        alt_text: 'marg'
+      };
+    }
+    return [
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `오늘 저녁은 <@${orderer.user}> 주문해주시겠어요? :pray: ${textMenuSuggestion}`
+        }
+      },
+      (orderButtonBlock && orderButtonBlock),
+      (menuImageBlock && menuImageBlock),
+    ];
   }
 }
